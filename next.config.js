9551c124adf143c4ab53b0e5f9a3b936f930927f
YuiFutao <yui.futao@gmail.com>
2021-12-03 18:31:19 +0900

    Hot Reloading対応
    https://weblion303.net/2515

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+
+  // Hot Reloading有効化
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
